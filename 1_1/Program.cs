397a04252ac84c678f10636b9d9c689fc6ba84e0
Kalinin <9776020@mail.ru>
2023-03-06 00:30:28 +0300

    Task1_1

---
+++ 1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int count_numbers(int num)
+{
+  int amount = 0;
+  for (int i = 1; num > 0; i++)
+  {
+    num = num / 10;
+    amount = i;
+
+  }
+  return amount;
+}
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(count_numbers(A));
