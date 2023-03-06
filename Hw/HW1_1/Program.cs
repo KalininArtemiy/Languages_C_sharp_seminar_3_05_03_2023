051fb6f940c3094cb83b90c62886b1d47ad2ac2a
Kalinin <9776020@mail.ru>
2023-03-07 00:30:17 +0300

    HW Task1_1

---
+++ Hw/HW1_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int number_degree(int number, int degree)
+{
+  int result = 1;
+  for (int i = 0; i < degree; i++)
+  {
+    result *= number;
+  }
+  return result;
+}
+Console.WriteLine(number_degree(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine())));
