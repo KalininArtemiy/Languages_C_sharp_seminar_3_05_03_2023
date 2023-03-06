cb6834daa0974ed5d71904d685ffcd6f7c80a7ae
Kalinin <9776020@mail.ru>
2023-03-07 01:10:55 +0300

    HW Task1_3

--- 1_3/Program.cs
+++ 1_3/Program.cs
@@ -2,11 +2,11 @@
 {
 int[] R = new int[leght];
 for (int i=0; i<leght; i++)
-{
-R[i] = new Random().Next(10);
-Console.WriteLine();
-Console.WriteLine(R[i]);
-}
+  {
+  R[i] = new Random().Next(10);
+  Console.WriteLine();
+  Console.WriteLine(R[i]);
+  }
 }
 
 Massiv(int.Parse(Console.ReadLine()!));
---
+++ Hw/HW1_3/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] new_massive(int Lenght, int Min, int Max)
+{
+  int[] array = new int[Lenght];
+  Random random = new Random();
+  for (int i = 0; i < Lenght; i++)
+  {
+    array[i] = random.Next(Min, Max+1);
+  }
+  return array;
+}
+
+void print(int[] array)
+{
+  Console.Write("[ ");
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]}, ");
+  }
+  Console.Write("]");
+}
+int[] arr = new_massive(8,1,10);
+print(arr);
